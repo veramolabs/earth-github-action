3064a6e09f86f767e11388067d6b5388f7fa50e8
Simonas Karuzas <simonas@not.cat>
2020-12-26 18:37:44 +0000

    record

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+
 import * as core from '@actions/core'
+import github from '@actions/github';
 import {agent} from './agent'
+const context = github.context;
 
 async function run(): Promise<void> {
   try {
@@ -7,6 +10,9 @@ async function run(): Promise<void> {
       alias: core.getInput('bot_alias'),
       provider: 'did:web'
     })
+
+    core.debug('context')
+    core.debug(JSON.stringify(context, null, 2))
 
     core.setOutput('did', bot.did)
   } catch (error) {
