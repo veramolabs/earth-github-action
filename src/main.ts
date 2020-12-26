0a643d6859467ed321b800b13808cd2fa7d1f28f
Simonas Karuzas <simonas@not.cat>
2020-12-26 18:27:45 +0000

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {agent} from './agent'
 async function run(): Promise<void> {
   try {
     const bot = await agent.didManagerGetOrCreate({
-      alias: core.getInput('bot_alias'), // sun.veramo.io:earth
+      alias: core.getInput('bot_alias'),
       provider: 'did:web'
     })
 
